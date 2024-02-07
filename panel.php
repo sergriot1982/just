f1102bcb3135df06f2474e85c4ba4240ca108a71
Sergey Kozelsky <158261415+sergriot1982@users.noreply.github.com>
2024-02-07 17:56:54 +0800

    Add files via upload

---
+++ panel.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>JUST | ADMIN PANEL</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='adminpanel.css'>
+
+</head>
+<body>
+    <h1>JUST ADMIN PANEL</h1>
+    
+    <form>
+        <button formaction="index.html" formtarget="_blank">GO TO WEB SITE</button>
+    </form>
+</body>
+</html>
